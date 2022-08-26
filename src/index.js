a47f1c888b5e9422501ea3da2aba7fe9c7f910d5
Igor Vieira <igorsv2240@gmail.com>
2022-08-26 14:43:37 -0300

    iniciando projeto

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+//init proj
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();

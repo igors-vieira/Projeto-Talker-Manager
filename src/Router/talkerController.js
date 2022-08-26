58e13ed0a464b35b3685d32d799fc5fdb4e7c195
Igor Vieira <igorsv2240@gmail.com>
2022-08-26 16:45:33 -0300

    requisito 2

--- src/Router/talkerController.js
+++ src/Router/talkerController.js
@@ -12,4 +12,18 @@ talkerR.get('/', async (_req, res) => {
   res.status(200).json(talkerParse);
 });
 
+talkerR.get('/:id', async (req, res) => {
+  const talkerJson = await fs.readFile(join(__dirname, '../talker.json'), { encoding: 'utf-8' });
+  
+  const talkerParse = JSON.parse(talkerJson);
+
+  const findId = talkerParse.find((talker) => talker.id === Number(req.params.id));
+
+  if (!findId) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(findId);
+});
+
 module.exports = talkerR;
